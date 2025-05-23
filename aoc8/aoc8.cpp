0d9cdc4ab4bbb4fc2ab55cb4a5344727889b5fb9
Paul <paul@epicyclism.com>
2025-05-23 11:57:07 +0100

    aoc8 pt1

--- aoc8/aoc8.cpp
+++ aoc8/aoc8.cpp
@@ -1,23 +1,80 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
 
 #include <fmt/format.h>
 
-#include "ctre_inc.h"
 #include "timer.h"
 
 auto get_input()
 {
-	return 0;
+	std::vector<std::string> rv;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+		rv.emplace_back(std::move(ln));
+	return rv;
 }
 
-int64_t pt1(auto const& in_addr_t)
+char from_hex(char c)
+{
+	char r = 0;
+	if(::isdigit(c))
+		return c - '0';
+	return c - 'a';
+}
+
+size_t quote_oh(std::string_view s)
+{
+	size_t l = 0;
+	int estate = 0;
+	for(auto c : s)
+	{
+		switch(estate)
+		{
+			case 0:
+				if(c == '\\')
+					estate = 1;
+				else
+				if(c != '"')
+					++l;
+				break;
+			case 1:
+				if(c == '\\' || c == '"')
+				{
+					++l;
+					estate = 0;
+				}
+				else
+				if(c == 'x')
+					++estate;
+				else
+					estate = 0; // bad input?
+				break;
+			case 2:
+				++estate;
+				break;
+			case 3:
+				++l;
+				estate = 0;
+				break;
+			default:
+				// bug
+				estate = 0;
+				break;
+		}
+	}
+	return s.size() - l;
+}
+
+int64_t pt1(auto const& in)
 {
 	timer t("p1");
-	return 0;
+	auto ql = std::ranges::fold_left(in, 0ULL, [](auto sm, auto const& s){ return sm + quote_oh(s);});
+
+	return ql;
 }
 
 int64_t pt2(auto const& in)
