857367e0d05998eb15f7cb164fbfb183cd22bb46
Paul <paul@epicyclism.com>
2025-05-24 16:44:39 +0100

    aoc10

--- aoc10/aoc10.cpp
+++ aoc10/aoc10.cpp
@@ -9,26 +9,54 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
-auto get_input()
+std::string transform(std::string_view s)
 {
-	return 0;
+	std::string sn;
+	auto c = s[0];
+	auto cnt = 0;
+	for(auto ch : s)
+	{
+		if (ch == c)
+			++cnt;
+		else
+		{
+			sn.push_back(cnt + '0');
+			sn.push_back(c);
+			cnt = 1;
+			c = ch;
+		}
+	}
+	if(cnt)
+	{
+		sn.push_back(cnt + '0');
+		sn.push_back(c);
+	}
+	return sn;
 }
 
-int64_t pt1(auto const& in_addr_t)
+int64_t pt1(auto const& in)
 {
 	timer t("p1");
-	return 0;
+	std::string proc = in;
+	for(int n = 0; n < 40; ++n)
+		proc = transform(proc);
+	return proc.size();
 }
 
 int64_t pt2(auto const& in)
 {
 	timer t("p2");
-	return 0;
+	std::string proc = in;
+	for(int n = 0; n < 50; ++n)
+		proc = transform(proc);
+	return proc.size();
 }
 
-int main()
+int main(int ac, char** av)
 {
-	auto in = get_input();
+	std::string in = "1113222113";
+	if(ac > 1)
+		in = av[1];
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
 	fmt::println("pt1 = {}", p1);
