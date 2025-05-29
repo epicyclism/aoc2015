0804fbaba91ea821d511ecce44cd8e8e16ff0ba2
Paul <paul@epicyclism.com>
2025-05-29 17:02:42 +0100

    aoc18

--- aoc18/aoc18.cpp
+++ aoc18/aoc18.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <bitset>
+#include <array>
+
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -9,21 +12,88 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
-auto get_input()
+template<int D> struct grid_t
 {
-	return 0;
+	std::bitset<(D+2)*(D+2)> bs_;
+	void set(int x, int y, bool b)
+	{
+		bs_.set(x + y * (D+2), b);
+	}
+	bool get(int x, int y) const
+	{
+		return bs_.test(x + y * (D+2));
+	}
+};
+
+grid_t<100> get_input()
+{
+	grid_t<100> bs;
+	std::string ln;
+	int y = 1;
+	while(std::getline(std::cin, ln))
+	{
+		int x = 1;
+		for(auto c: ln)
+		{
+			bs.set(x, y, c == '#');
+			++x;
+		}
+		++y;
+	}
+	fmt::println("got {} bits", bs.bs_.count());
+	return bs;
 }
 
-int64_t pt1(auto const& in_addr_t)
+constexpr std::pair<int, int> offsets [] = { {-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
+
+int cnt_neighbours(grid_t<100> const& g, int x, int y)
+{
+	int c = 0;
+	for(auto& xy: offsets)
+		c += g.get(x + xy.first, y + xy.second);
+	return c;
+}
+
+grid_t<100> step(grid_t<100> const& g)
+{
+	grid_t<100> nxt;
+	for(int y = 1; y < 101; ++y)
+		for(int x = 1; x < 101; ++x)
+		{
+			auto neighbours = cnt_neighbours(g, x, y);
+			if(g.get(x, y))
+				nxt.set(x, y, neighbours == 2 || neighbours == 3);
+			else
+				nxt.set(x, y, neighbours == 3);
+		}
+	return nxt;
+}
+
+int pt1(auto const& in)
 {
 	timer t("p1");
-	return 0;
+	grid_t<100> wking(in);
+	for(int n = 0; n < 100; ++n)
+	{
+		wking = step(wking);
+	}
+	return wking.bs_.count();
 }
 
-int64_t pt2(auto const& in)
+constexpr std::pair<int, int> corners [] = { {1, 1}, {100, 1}, {1, 100}, {100, 100}};
+
+int pt2(auto const& in)
 {
 	timer t("p2");
-	return 0;
+	grid_t<100> wking(in);
+	for(int n = 0; n < 100; ++n)
+	{
+		wking = step(wking);
+		for(auto& xy: corners)
+			wking.set(xy.first, xy.second, true);
+		
+	}
+	return wking.bs_.count();
 }
 
 int main()
