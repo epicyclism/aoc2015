324a377cd24f5b2d4eaaa806a632102f53fa8322
Paul <paul@epicyclism.com>
2025-05-31 18:10:24 +0100

    aoc20

--- aoc20/aoc20.cpp
+++ aoc20/aoc20.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <vector>
 
 #include <fmt/format.h>
 
 #include "ctre_inc.h"
 #include "timer.h"
 
-auto get_input()
-{
-	return 0;
-}
-
-int64_t pt1(auto const& in_addr_t)
+int pt1(auto const& in)
 {
 	timer t("p1");
+	std::vector<int> houses(in / 20, 0);
+	for(int e = 1; e < houses.size(); ++e)
+	{
+		for( int h = e; h < houses.size(); h += e)
+			houses[h] += e * 10;
+	}
+	for(int h = 0; h < houses.size(); ++h)
+		if( houses[h] >= in)
+			return h;
 	return 0;
 }
 
-int64_t pt2(auto const& in)
+int pt2(auto const& in)
 {
 	timer t("p2");
+	std::vector<int> houses(in / 20, 0);
+	for(int e = 1; e < houses.size(); ++e)
+	{
+		int cnt = 0; int h = e;
+		for( ; cnt < 50 && h < houses.size(); ++cnt, h += e)
+			houses[h] += e * 11;
+	}
+	for(int h = 0; h < houses.size(); ++h)
+		if( houses[h] >= in)
+			return h;
 	return 0;
 }
 
-int main()
+int main(int ac, char** av)
 {
-	auto in = get_input();
+	auto in = 33100000;
+	if(ac > 1)
+		in = sv_to_t<int>(av[1]);
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
 	fmt::println("pt1 = {}", p1);
