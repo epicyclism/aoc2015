29a840800d787f11badecfcfa48263078204a9ca
Paul <paul@epicyclism.com>
2025-06-11 20:03:08 +0100

    sort run_all

--- run_all.sh
+++ run_all.sh
@@ -6,21 +6,21 @@ build/aoc2/aoc2bb < inputs/aoc2/input
 echo "day 3"
 build/aoc3/aoc3 < inputs/aoc3/input
 echo "day 4"
-build/aoc4/aoc4 < inputs/aoc4/input
+build/aoc4/aoc4
 echo "day 5"
 build/aoc5/aoc5 < inputs/aoc5/input
 echo "day 6"
 build/aoc6/aoc6 < inputs/aoc6/input
 echo "day 7"
-build/aoc7/aoc7r < inputs/aoc7/input
+build/aoc7/aoc7 < inputs/aoc7/input
 echo "day 8"
 build/aoc8/aoc8 < inputs/aoc8/input
 echo "day 9"
 build/aoc9/aoc9 < inputs/aoc9/input
 echo "day 10"
-build/aoc10/aoc10 < inputs/aoc10/input
+build/aoc10/aoc10
 echo "day 11"
-build/aoc11/aoc11 < inputs/aoc11/input
+build/aoc11/aoc11
 echo "day 12"
 build/aoc12/aoc12 < inputs/aoc12/input
 echo "day 13"
@@ -38,7 +38,7 @@ build/aoc18/aoc18 < inputs/aoc18/input
 echo "day 19"
 build/aoc19/aoc19 < inputs/aoc19/input
 echo "day 20"
-build/aoc20/aoc20 < inputs/aoc20/input
+build/aoc20/aoc20
 echo "day 21"
 build/aoc21/aoc21 < inputs/aoc21/input
 echo "day 22"
